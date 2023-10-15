d8b8017d5422a9396b7c0544a2b088e2d4f5e90b
moshiuzzaman <md.shozon041@gmail.com>
2023-10-15 13:05:51 +0600

    refactor(server): initial setup

---
+++ src/enums/user.ts
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+export enum ENUM_USER_ROLE {
+  SUPER_ADMIN = 'SUPER_ADMIN',
+  ADMIN = 'ADMIN',
+  USER = 'USER',
+}
+
+export enum ENUM_STATUS {
+  PENDING = 'PENDING',
+  APPROVED = 'APPROVED',
+  REJECTED = 'REJECTED',
+  DONE = 'DONE',
+}
